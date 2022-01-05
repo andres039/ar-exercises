d7e009db6dcf27064c367cdbaf55416d1720906e
Andres <andres039039@Hotmail.com>
2022-01-05 02:47:32 +0000

    exercise 7 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,7 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+puts "Here is the test:#{@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60).valid?}"
 @store2.employees.create(first_name: "Bob", last_name: "Burguers", hourly_rate: 20)
 @store1.employees.create(first_name: "Bertrand", last_name: "Gilffoyle", hourly_rate: 85)
 @store2.employees.create(first_name: "Jack", last_name: "Sparrow", hourly_rate: 10)
